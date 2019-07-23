c79e64efb9b3bdbc07989fb1d1ca7c785fbf1702
LudovicoLowe <lowe.l@hotmail.com>
2019-07-23 13:57:26 +0200

    init

---
+++ IGEA_Source/src/main/java/igeagroup/IgeaSourceApplication.java
@@ -0,0 +1,13 @@
+package igeagroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgeaSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgeaSourceApplication.class, args);
+	}
+
+}
---
+++ IGEA_Source/src/test/java/igeagroup/IgeaSourceApplicationTests.java
@@ -0,0 +1,16 @@
+package igeagroup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IgeaSourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
