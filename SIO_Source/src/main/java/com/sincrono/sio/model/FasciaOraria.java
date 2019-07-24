5c52541f73a51dd5c1bc7de85ce0aa2b1098b813
LorenzoMugnari <51627018+LorenzoMugnari@users.noreply.github.com>
2019-07-24 13:38:17 +0200

    push l

---
+++ SIO_Source/src/main/java/com/sincrono/sio/model/FasciaOraria.java
@@ -0,0 +1,41 @@
+package com.sincrono.sio.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="fascia_oraria")
+public class FasciaOraria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id_fascia_oraria;
+	String fascia;
+	
+	public FasciaOraria(int id_fascia_oraria, String fascia) {
+		super();
+		this.id_fascia_oraria = id_fascia_oraria;
+		this.fascia = fascia;
+	}
+	public FasciaOraria() {}
+	public int getId_fascia_oraria() {
+		return id_fascia_oraria;
+	}
+	public void setId_fascia_oraria(int id_fascia_oraria) {
+		this.id_fascia_oraria = id_fascia_oraria;
+	}
+	public String getFascia() {
+		return fascia;
+	}
+	public void setFascia(String fascia) {
+		this.fascia = fascia;
+	}
+	
+	
+	
+	
+
+}
